f077f35ed23f1d50e50d8a79a1a77c631799bd8a
andreipantea <andrei.p@seomonitor.com>
2021-05-03 13:04:24 +0200

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,81 @@
 <template>
   <v-app>
 
-      <v-card
-          height="400"
-          width="256"
-          class="mx-auto"
-      >
-          <v-navigation-drawer permanent>
-              <v-list-item>
+      <v-navigation-drawer app :mobile-breakpoint="0">
+          <!-- -->
+          <v-list-item>
+              <v-list-item-content>
+                  <v-list-item-title class="title">
+                      Application
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                      subtext
+                  </v-list-item-subtitle>
+              </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+              dense
+              nav
+          >
+              <v-list-item
+                  v-for="item in items"
+                  :key="item.title"
+                  link
+              >
+                  <v-list-item-icon>
+                      <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+
                   <v-list-item-content>
-                      <v-list-item-title class="title">
-                          Application
-                      </v-list-item-title>
-                      <v-list-item-subtitle>
-                          subtext
-                      </v-list-item-subtitle>
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
                   </v-list-item-content>
               </v-list-item>
+          </v-list>
+      </v-navigation-drawer>
 
-              <v-divider></v-divider>
+      <v-app-bar app>
+          <!-- -->
+      </v-app-bar>
 
-              <v-list
-                  dense
-                  nav
-              >
-                  <v-list-item
-                      v-for="item in items"
-                      :key="item.title"
-                      link
-                  >
-                      <v-list-item-icon>
-                          <v-icon>{{ item.icon }}</v-icon>
-                      </v-list-item-icon>
+      <!-- Sizes your content based upon application components -->
+      <v-main>
 
-                      <v-list-item-content>
-                          <v-btn :to="item.to">{{ item.title }}</v-btn>
-                      </v-list-item-content>
-                  </v-list-item>
-              </v-list>
-          </v-navigation-drawer>
-      </v-card>
-    <v-main>
-        <v-container fluid>
-            <router-view>
-                <Home/>
-                <About/>
-            </router-view>
-        </v-container>
-    </v-main>
+          <!-- Provides the application the proper gutter -->
+          <v-container fluid>
+
+              <!-- If using vue-router -->
+              <router-view></router-view>
+          </v-container>
+      </v-main>
+
+      <v-footer app>
+          <!-- -->
+      </v-footer>
   </v-app>
 </template>
 
 <script>
-import About from "@/views/About";
-import Home from "@/views/Home";
+// import About from "@/views/About";
+// import Home from "@/views/Home";
 
 export default {
   name: 'App',
 
   components: {
-      Home,
-      About,
+      // Home,
+      // About,
   },
 
   data: () => ({
       items: [
-          { title: 'Home', icon: 'mdi-view-dashboard', to: '/' },
-          { title: 'About', icon: 'mdi-image', to: '/about' },
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
       ],
+      right: null,
   }),
 };
 </script>
